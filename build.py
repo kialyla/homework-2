651feb3b12021025c26d36a7adcdf63ed27d9602
Kia Combest <kia.lyla@gmail.com>
2018-09-19 17:04:45 -0700

    edited build.py

--- build.py
+++ build.py
@@ -4,15 +4,15 @@ bottom = open('templates/bottom.html').read()
 content = open('content/index.html').read()
 
 index_html = top + content + bottom
-open('index.html', 'w+').write(index_html)
+open('docs/index.html', 'w+').write(index_html)
 
 content = open('content/projects.html').read()
 
 projects_html = top + content + bottom
-open('projects.html', 'w+').write(projects_html)
+open('docs/projects.html', 'w+').write(projects_html)
 
 content = open('content/resume.html').read()
 
 resume_html = top + content + bottom
-open('resume.html', 'w+').write(resume_html)
+open('docs/resume.html', 'w+').write(resume_html)
 
